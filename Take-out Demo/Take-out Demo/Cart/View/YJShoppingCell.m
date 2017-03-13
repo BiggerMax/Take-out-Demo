ca2ecae627cbe786dfc7c00003d7f6e7cbe45967
袁杰 <yuanjiegogo@163.com>
2017-03-13 09:32:36 +0800

    312

--- Take-out Demo/Take-out Demo/Cart/View/YJShoppingCell.m
+++ Take-out Demo/Take-out Demo/Cart/View/YJShoppingCell.m
@@ -8,18 +8,70 @@
 
 #import "YJShoppingCell.h"
 #import <UIButton+WebCache.h>
+#import "YJShopCarTool.h"
+#import "YJBuyView.h"
 
+@interface YJShoppingCell()
+@property(nonatomic,strong) UIButton *selectedBtn;
+@property(nonatomic,strong) UIImageView *shoppingIcon;
+@property(nonatomic,strong) UILabel *siftLabel;
+@property(nonatomic,strong) UILabel *shoppingName;
+@property(nonatomic,strong) UILabel *moneyLabel;
+@property(nonatomic,strong) YJBuyView *buyView;
+@end
 @implementation YJShoppingCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        _selectedBtn = [UIButton new];
+        _selectedBtn.selected = YES;
+        [_selectedBtn setImage:[UIImage imageNamed:@"v2_noselected"] forState:UIControlStateNormal];
+        [_selectedBtn setImage:[UIImage imageNamed:@"v2_selected"] forState:UIControlStateSelected];
+        [_selectedBtn addTarget:self action:@selector(shoppingSelectedClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        _shoppingIcon = [UIImageView new];
+        _siftLabel = [UILabel new];
+        _siftLabel.font = [UIFont systemFontOfSize:11];
+        _siftLabel.layer.borderWidth = 1;
+        _siftLabel.layer.borderColor = [UIColor redColor].CGColor;
+        _siftLabel.textColor = [UIColor redColor];
+        _siftLabel.layer.cornerRadius = 3;
+        _siftLabel.layer.masksToBounds = YES;
+        _siftLabel.textAlignment = NSTextAlignmentCenter;
+        _siftLabel.text = @"精选";
+        _siftLabel.hidden = YES;
+        
+        _shoppingName = [UILabel new];
+        _shoppingName.font = [UIFont systemFontOfSize:15];
+        _shoppingName.textAlignment = NSTextAlignmentLeft;
+        
+        _moneyLabel = [UILabel new];
+        _moneyLabel.font = [UIFont systemFontOfSize:15];
+        _moneyLabel.textAlignment = NSTextAlignmentLeft;
+        _moneyLabel.textColor = [UIColor redColor];
+        
+        _buyView = [YJBuyView new];
+        
+        [self addSubview:_selectedBtn];
+        [self addSubview:_siftLabel];
+        [self addSubview:_shoppingName];
+        [self addSubview:_shoppingIcon];
+        [self addSubview:_moneyLabel];
+        [self addSubview:_buyView];
+        
+        [_selectedBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.height.with.mas_equalTo(15);
+            make.leading.equalTo(self).offset(10);
+            make.centerY.equalTo(self);
+        }];
+        [_shoppingIcon mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.height.width.mas_equalTo(50);
+            make.leading.equalTo(_selectedBtn.mas_trailing).offset(5);
+            make.centerY.equalTo(self);
+
+        }];
+        
+    }
 }
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
 @end
